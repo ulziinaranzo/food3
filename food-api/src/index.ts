d43dc17e90fecc29d4730c4acd8d8b686101b589
24LP2564 24LP2564 <24LP2564@G401-CC0116.local>
2025-04-21 20:19:30 +0800

    loveasdf

--- food-api/src/index.ts
+++ food-api/src/index.ts
@@ -1,21 +1,22 @@
 import express from "express";
 import foodRouter from "./routes/food.route";
-import categoryRouter from "./routes/category.route"; 
+import categoryRouter from "./routes/category.route";
 import { connectToDatabase } from "./database/connect-to-db";
 import userRouter from "./routes/user.route";
 import foodOrderRouter from "./routes/food-order.route";
-
+import cors from "cors";
 connectToDatabase();
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use(foodRouter);
 app.use(categoryRouter);
 app.use(userRouter);
 app.use("/food-order", foodOrderRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
